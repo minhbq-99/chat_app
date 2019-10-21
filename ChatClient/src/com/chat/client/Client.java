3a3ca77ded64b2b475d35c912864293b88e0ce20
Bui Quang Minh <minhquangbui99@hotmail.com>
2019-10-21 14:49:39 +0700

    add client send function example

--- ChatClient/src/com/chat/client/Client.java
+++ ChatClient/src/com/chat/client/Client.java
@@ -1,5 +1,20 @@
 package com.chat.client;
+import java.net.*;
+import java.io.*;
 
 public class Client {
-
+	public static boolean sendLogin(Socket socket, String username, String password)
+	{
+		try
+		{
+			String message = "LOGIN\n" + username + "\n" + password + "\n";
+			OutputStream output = socket.getOutputStream();
+			output.write(message.getBytes());
+		}
+		catch(IOException ex)
+		{
+			return false;
+		}
+		return true;
+	}
 }
